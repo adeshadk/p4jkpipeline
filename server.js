c1def118fa262074a034b2e58a5d44e30344e284
adeshadk <adeshak@hotmail.com>
2019-02-17 21:14:58 -0800

    commit server.js:201902172113

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('URL requested: ' + request.url);
   response.writeHead(200);
-  response.end('This is simplilearn project 4 for Devops Practioner Course on Feb 16 at 11:00am');
+  response.end('This is simplilearn project 4 for Devops Practioner Course on Feb 17 at 9:13am');
 };
 var www = http.createServer(handleRequest);
 www.listen(8081);
